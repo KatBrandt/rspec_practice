e7ab672b0801853bfedf15c57898e93a54150133
Kat Brandt <joyfulkat11@gmail.com>
2021-12-31 21:57:48 -0700

    adds test and code for default name.

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,20 @@
 class Student
-  attr_reader :name
+  attr_reader :name, :cookies
   #
   def initialize(name)
-    @name = name
+    @name = check_name(name)
     @cookies = []
+  end
+
+  def add_cookie(cookie)
+    @cookies << cookie
+  end
+
+  def check_name(name)
+    if name.is_a?(String)
+      name
+    else
+      "Default Name Assigned"
+    end
   end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -12,5 +12,26 @@ describe Student do
       student = Student.new('Bob')
       expect(student.name).to eq 'Bob'
     end
+
+    it 'assigns a default name' do
+      student = Student.new(42)
+      expect(student.name).to eq "Default Name Assigned"
+    end
+
+    it 'has no cookies by default' do
+      student = Student.new('Bob')
+      expect(student.cookies).to eq []
+    end
+  end
+
+  describe '#add_cookie' do
+    it 'adds cookie to cookies array' do
+      student = Student.new('Bob')
+      student.add_cookie('Choc Chip')
+      student.add_cookie('Snickerdoodle')
+      cookies = ['Choc Chip', 'Snickerdoodle']
+
+      expect(student.cookies).to eq(cookies)
+    end
   end
 end
